b16bddaef5376a72ddebd954a9a7676a021483d3
Quinton Holmes <quintonrholmes30@gmail.com>
2026-03-26 19:40:25 -0400

    sure

--- app/AnimateMe/AnimateMe/HomePage.xaml.cs
+++ app/AnimateMe/AnimateMe/HomePage.xaml.cs
@@ -2,34 +2,38 @@ namespace AnimateMe;
 
 public partial class HomePage : ContentPage
 {
-	public HomePage()
-	{
-		InitializeComponent();
-	}
+    public HomePage()
+    {
+        InitializeComponent();
+    }
 
-	async void OnUploadFile(object sender, EventArgs args)
-	{
-		var result = await FilePicker.PickAsync(new PickOptions
-		{
-			PickerTitle = "pickerImage",
-			FileTypes = FilePickerFileType.Images
+    async void OnUploadFile(object sender, EventArgs args)
+    {
+        var result = await FilePicker.PickAsync(new PickOptions
+        {
+            PickerTitle = "pickerImage",
+            FileTypes = FilePickerFileType.Images
         });
 
-		if (result is not null)
-		{
-			var stream = await result.OpenReadAsync();
+        if (result is not null)
+        {
+            var stream = await result.OpenReadAsync();
 
-			imageDisplay.Source = ImageSource.FromStream(() => stream);
+            byte[] imageData;
 
-			// use stream, communicate with python here
-			using (MemoryStream memoryStream = new MemoryStream())
-			{
-				stream.CopyTo(memoryStream);
-				byte[] imageData = memoryStream.ToArray();
-				string imageString = Convert.ToBase64String(imageData);
+            // use stream, communicate with python here
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                await stream.CopyToAsync(memoryStream);
+                imageData = memoryStream.ToArray();
+            }
 
-				// send to api call here
-			}
-		}
+            imageDisplay.Source = ImageSource.FromStream(() => new MemoryStream(imageData));
+
+            string imageString = Convert.ToBase64String(imageData);
+
+            // send to api call here
+
+        }
     }
 }
