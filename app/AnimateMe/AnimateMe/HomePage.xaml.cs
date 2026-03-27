1c9f2930b5ec37dd9b1357abbf2a69ef5ccedf5a
Quinton Holmes <quintonrholmes30@gmail.com>
2026-03-27 18:52:04 -0400

    fix this

--- app/AnimateMe/AnimateMe/HomePage.xaml.cs
+++ app/AnimateMe/AnimateMe/HomePage.xaml.cs
@@ -1,3 +1,9 @@
+using System.Diagnostics;
+using System.Text;
+using IronPython;
+using IronPython.Runtime;
+using Microsoft.Scripting.Hosting;
+
 namespace AnimateMe;
 
 public partial class HomePage : ContentPage
@@ -32,8 +38,58 @@ public partial class HomePage : ContentPage
 
             string imageString = Convert.ToBase64String(imageData);
 
+            ScriptEngine engine = IronPython.Hosting.Python.CreateEngine();
+
+            var debugStream = new DebugStream();
+
+            engine.Runtime.IO.SetOutput(debugStream, Encoding.UTF8);
+
+            string pyScript = "import math\nresult = math.sqrt(25)\nprint(\"The result: \" + str(result))";
+
+            ScriptSource scriptSource = engine.CreateScriptSourceFromString(pyScript);
+
+            var pyResult = scriptSource.Execute();
+
             // send to api call here
 
+        }
+    }
+}
+
+public class DebugStream : Stream
+{
+    private readonly MemoryStream _buffer = new MemoryStream();
+
+    public override bool CanRead => false;
+    public override bool CanSeek => false;
+    public override bool CanWrite => true;
+    public override long Length => 0;
+    public override long Position { get => 0; set { } }
+
+    public override void Flush()
+    {
+        if (_buffer.Length > 0)
+        {
+            string text = Encoding.UTF8.GetString(_buffer.ToArray());
+            System.Diagnostics.Debug.Write(text);
+            _buffer.SetLength(0);
+        }
+    }
+
+    public override int Read(byte[] buffer, int offset, int count) => 0;
+    public override long Seek(long offset, SeekOrigin origin) => 0;
+    public override void SetLength(long value) { }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        // Append to buffer
+        _buffer.Write(buffer, offset, count);
+        string utf8String = Encoding.UTF8.GetString(buffer);
+
+        // If newline detected, flush
+        if (buffer[offset + count - 1] == (byte)'\n')
+        {
+            Flush();
         }
     }
 }
